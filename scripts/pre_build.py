3a17c287e3f9950bdf72a3f5a7ea632dd4b18b98
Joseph Courtney <joseph.m.courtney@gmail.com>
2024-05-31 15:08:29 -0400

    build: get pre-commit hooks working for linting, formatting, testing, and type checking.

--- scripts/pre_build.py
+++ scripts/pre_build.py
@@ -1,21 +1,30 @@
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-import subprocess
+# ruff: noqa: INP001 # there is no need to import anything scripts/
+import subprocess  # noqa: S404
 import sys
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 
 class SpecialBuildHook(BuildHookInterface):
     PLUGIN_NAME = "pre_build"
 
-    def initialize(self, version, build_data):
+    @staticmethod
+    def initialize(version, build_data):  # noqa: ARG004 # overrides externally defined method
         try:
-            command = ["configle", "~/.config/mypy/config/global.ini", "./mypy.local.ini", "-o", "mypy.ini"]
+            command = [
+                "~/.local/bin/configle",
+                "~/.config/mypy/config/global.ini",
+                "./mypy.local.ini",
+                "-o",
+                "mypy.ini",
+            ]
             print(f"build.py: {" ".join(command)}")
-            result = subprocess.run(command, check=True)
+            result = subprocess.run(command, check=True)  # noqa: S603
             if result.returncode != 0:
                 print("build.py: script returned non-zero exit code.", file=sys.stderr)
                 sys.exit(result.returncode)
 
-            result = subprocess.run(["git", "add", "mypy.ini"], check=True)
+            result = subprocess.run(["/usr/bin/git", "add", "mypy.ini"], check=True)  # noqa: S603
 
         except subprocess.CalledProcessError as e:
             print(f"build.py: subprocess exception: {e}", file=sys.stderr)
--- src/orangutan/utils/helpers.py
+++ src/orangutan/utils/helpers.py
@@ -1,5 +1,1 @@
 """Utility functions and helper methods."""
-
-
-def sample_helper_function(param: str) -> str:
-    return param.upper()
