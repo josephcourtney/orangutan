c4800dc4ff30f6b2c774ea6da72f8db78e32b08d
Joseph Courtney <joseph.m.courtney@gmail.com>
2024-05-31 14:29:34 -0400

    build: fix build script

--- scripts/pre_build.py
+++ scripts/pre_build.py
@@ -1,19 +1,22 @@
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 import subprocess
 import sys
 
 
-def pre_build():
-    try:
-        command = ["configle", "~/.config/mypy/config/global.ini", "./mypy.local.ini", "-o", "mypy.ini"]
-        print(f"build.py: {" ".join(command)}")
-        result = subprocess.run(command, check=True)
-        if result.returncode != 0:
-            print("build.py: script returned non-zero exit code.", file=sys.stderr)
-            sys.exit(result.returncode)
-    except subprocess.CalledProcessError as e:
-        print(f"build.py: subprocess exception: {e}", file=sys.stderr)
-        sys.exit(e.returncode)
+class SpecialBuildHook(BuildHookInterface):
+    PLUGIN_NAME = "pre_build"
 
+    def initialize(self, version, build_data):
+        try:
+            command = ["configle", "~/.config/mypy/config/global.ini", "./mypy.local.ini", "-o", "mypy.ini"]
+            print(f"build.py: {" ".join(command)}")
+            result = subprocess.run(command, check=True)
+            if result.returncode != 0:
+                print("build.py: script returned non-zero exit code.", file=sys.stderr)
+                sys.exit(result.returncode)
 
-if __name__ == "__main__":
-    pre_build()
+            result = subprocess.run(["git", "add", "mypy.ini"], check=True)
+
+        except subprocess.CalledProcessError as e:
+            print(f"build.py: subprocess exception: {e}", file=sys.stderr)
+            sys.exit(e.returncode)
